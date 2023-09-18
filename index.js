22a208e536edc070b84500ea018d0d31c59d942a
Chiko repo <124736255+chikogit@users.noreply.github.com>
2023-09-17 21:02:56 -0400

    js-index-addition

--- index.js
+++ index.js
@@ -2,3 +2,8 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const generateMarkdown = require('./Develop/utils/generateMarkdown');
+
+// TODO: Create an array of questions for user input
+const questions = () => {
+    return inquirer.prompt([
+      {  
