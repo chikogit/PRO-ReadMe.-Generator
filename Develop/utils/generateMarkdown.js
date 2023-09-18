c7ee83629fc1235b53077f53e8c02ca6db7f65ff
Chiko repo <124736255+chikogit@users.noreply.github.com>
2023-09-17 20:11:53 -0400

    License-addition

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -15,3 +15,12 @@ const renderLicenseBadge = (license) => {
     return `- [License](#License)`
   }
  };
+ const renderLicenseSection = (license) => {
+  if (license === "None") {
+    return "";
+  } else {
+    return `## License
+    
+    This application is covered by the ${license} license.`
+  }
+}
