38961a6281ff724bf743a12268c9c1e6d99235e5
Chiko repo <124736255+chikogit@users.noreply.github.com>
2023-09-17 20:13:11 -0400

    License-addition

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -24,3 +24,47 @@ const renderLicenseBadge = (license) => {
     This application is covered by the ${license} license.`
   }
 }
+
+function generateMarkdown(data) {
+  return `
+  # ${data.title}
+
+  ${renderLicenseBadge(data.license)}
+
+  ## Description
+  ${data.description}
+
+  ## Table of Contents
+  - [Description](#Description)
+  - [Installation](#Installation)
+  - [Usage](#Usage)
+  - [Contributing](#Contributing)
+  - [Tests](#Tests)
+  ${renderLicenseLink(data.license)}
+  - [Questions](#Questions)
+
+  ## Installation
+  ${data.installation}
+
+  ## Usage
+  ${data.usage}
+
+  ## Contributing
+  ${data.contributions}
+
+  ## Tests
+  ${data.test}
+
+  ${renderLicenseSection(data.license)}
+  ${renderLicenseBadge(data.license)}
+
+  ## Questions
+  If you have any questions regarding this project, feel free to reach out to the GitHub username and/or email below:
+
+  - GitHub: https://github.com/${data.github}
+  - Email address: ${data.email}
+
+`;
+}
+
+module.exports = generateMarkdown;
