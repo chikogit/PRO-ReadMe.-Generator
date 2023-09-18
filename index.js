92d07a29d13fe2c355d891da84bb5be653e033f7
Chiko repo <124736255+chikogit@users.noreply.github.com>
2023-09-17 21:06:11 -0400

    type-name-message-validate-JS-addition

--- index.js
+++ index.js
@@ -2,8 +2,20 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const generateMarkdown = require('./Develop/utils/generateMarkdown');
-
 // TODO: Create an array of questions for user input
 const questions = () => {
     return inquirer.prompt([
       {  
+        //   project title is entered first and is required - shown as ${data.title} in generateMarkdown file
+        type: 'input',
+        name: 'title',
+        message: 'Hello! Please enter your project name. (Required)', 
+        validate: projectTitle => {
+            if (projectTitle) {
+                return true;
+            } else {
+                console.log("Please enter the title to your project!");
+                return false;
+            }
+            }
+        },
